1fb4f3d2f8991c77205a8e39ba2e0c2bf3a3497a
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-12-21 00:51:24 +0200

    Created the presentation layer

---
+++ src/main/java/com/elvis/springapp/ordermanager/domain/Order.java
@@ -0,0 +1,42 @@
+package com.elvis.springapp.ordermanager.domain;
+
+public class Order implements Identifiable<Long>{
+    private Long id;
+    private String description;
+    private long costInCents;
+    private boolean isComplete;
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCostInCents() {
+        return costInCents;
+    }
+
+    public void setCostInCents(long costInCents) {
+        this.costInCents = costInCents;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+}
