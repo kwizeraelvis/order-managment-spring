41bceada6133716dabbdf1d95783c90d456d51f2
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-12-21 00:16:24 +0200

    Complete Project BootStrap

---
+++ src/main/java/com/elvis/springapp/ordermanager/server/main/OrdermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.elvis.springapp.ordermanager.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdermanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elvis/springapp/ordermanager/server/main/OrdermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.springapp.ordermanager.server.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
